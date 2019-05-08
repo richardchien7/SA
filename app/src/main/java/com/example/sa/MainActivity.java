55ce05ab1db7f8b58110ab619f684401452d3c67
richardchien7 <richardchien6@gmail.com>
2019-05-08 15:15:54 +0800

    2019/5/8

--- app/src/main/java/com/example/sa/MainActivity.java
+++ app/src/main/java/com/example/sa/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.sa;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text =(TextView) findViewById(R.id.text);
+        text.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 }
