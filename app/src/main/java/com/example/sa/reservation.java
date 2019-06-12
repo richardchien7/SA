eee913130aa929972df678f73b9ddba6ccd93c8a
Jennyangs <hollister0330827@gmail.com>
2019-06-12 22:25:08 +0800

    Merge branch 'master' of https://github.com/richardchien7/SA
    # Conflicts:
    #	app/src/main/java/com/example/sa/choose_division.java

---
+++ app/src/main/java/com/example/sa/reservation.java
@@ -0,0 +1,45 @@
+package com.example.sa;
+
+public class reservation {
+
+    private String division_name;
+    private String doctor_name;
+    private String office;
+    private String visit_date;
+    private String vist_period;
+    private String visit_time_id;
+    private String patient_id;
+
+    public void setDivision_name(String division_name) {
+        this.division_name = division_name;
+    }
+
+    public void setDoctor_name(String doctor_name) {
+        this.doctor_name = doctor_name;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public void setVisit_date(String visit_date) {
+        this.visit_date = visit_date;
+    }
+
+    public void setVist_period(String vist_period) {
+        this.vist_period = vist_period;
+    }
+
+    public void setVisit_time_id(String visit_time_id) {
+        this.visit_time_id = visit_time_id;
+    }
+
+    public void setPatient_id(String patient_id) {
+        this.patient_id = patient_id;
+    }
+
+
+
+
+
+}
