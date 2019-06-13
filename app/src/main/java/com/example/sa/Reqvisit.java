2be4a142d1cae15c99eb14edc0a8cfdc522486eb
Jennyangs <hollister0330827@gmail.com>
2019-06-13 14:31:45 +0800

    Merge branch 'master' of https://github.com/richardchien7/SA
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/sa/choose_division.java

---
+++ app/src/main/java/com/example/sa/Reqvisit.java
@@ -0,0 +1,22 @@
+package com.example.sa;
+
+public class Reqvisit {
+    public String id ;
+
+    private fieldsvisit fields;
+
+    public String getId(){return id;}
+
+    public Reqvisit(fieldsvisit fields)
+    {
+        this.fields = fields;
+    }
+
+
+
+
+
+    public fieldsvisit getFields() {
+        return fields;
+    }
+}
